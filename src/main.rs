bd0491c6cf0348fe98b27f0426ef8f489ebfd451
rektrex <psaimanideep98@gmail.com>
2019-07-27 12:21:07 +0530

    move command line argument extraction logic

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(&config.filename)
         .expect("Something went wrong reading the file");
 
-    println!("Text in {}:\n{}", filename, contents);
+    println!("Contents of {}:\n{}", config.filename, contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }

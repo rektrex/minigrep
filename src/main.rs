bfeb61ee84bd1740a43f44d822096cdb7a57bffd
rektrex <psaimanideep98@gmail.com>
2019-07-27 17:14:37 +0530

    handle error returned from run in main

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ fn main() {
         process::exit(1);
     });
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {

8bbf070d49319c4f5483d95c10c8c7bf184a8b0b
rektrex <psaimanideep98@gmail.com>
2019-07-27 11:10:52 +0530

    read and store file contents

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,8 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("query: {}", query);
-    println!("filename: {}", filename);
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("Text in {}:\n{}", filename, contents);
 }
